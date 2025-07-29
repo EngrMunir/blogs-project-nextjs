16e6fc6b1f574372aee2ad8fd93ccc00cc8f5c3b
munir <smsirajulmonir@gmail.com>
2025-07-29 17:10:39 +0600

    revalidate ssr applied

---
+++ src/app/blogs/loading.tsx
@@ -0,0 +1,11 @@
+import Spinner from '@/components/ui/Spinner';
+
+const BlogLoadingPage = async() => {
+    return (
+        <div>
+            <Spinner/>
+        </div>
+    );
+};
+
+export default BlogLoadingPage;
